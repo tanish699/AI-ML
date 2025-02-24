0c5723144fb81d96c1e536b913c29dd17cf1a5d8
tanish699 <tanishwadhwa2310@gmail.com>
2025-02-24 11:56:05 +0530

    learned Basics

---
+++ Python/Basics/Numbers.py
@@ -0,0 +1,14 @@
+x= 2+2
+print(x)
+x= 2-2
+print(x)
+x= 4/2
+print(x)
+x= 8%3
+print(x)
+x= 2**3 #Power
+print(x)
+# x= 2*2
+# print(x)
+# x= 2*2
+# print(x)
---
+++ Python/Basics/Strings.py
@@ -0,0 +1,23 @@
+a ='Tanish'
+b = "Wadhwa"
+
+# Both are correct
+print("Hellp \n World")
+print(len(a))
+
+#--------------Indexing&Slicing--------------#
+#Indexing
+print(a[0])
+print(b[-6])
+
+#Slicing
+print(a[2:])
+print(b[1:4])
+
+#StepSize
+print(a[::2])  #Skips the letters
+
+#Reverse a string
+print(a[::-1])
+
+#--------------Immutability--------------#
